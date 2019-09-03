2f03c8b8889163a4983dec4ac5bc0cd773c18735
Robert Altnoeder <robert.altnoeder@linbit.com>
2019-09-03 15:08:07 +0200

    Additional unit tests

--- unit_test.go
+++ unit_test.go
@@ -12,6 +12,64 @@ var m = map[string]int64{
 	"MiB": 1024 * 1024,
 }
 
+var mapTests = []struct {
+	description string
+	mapping     map[string]int64
+	expectFail  bool
+}{
+	{
+		description: "Valid map 1",
+		mapping: map[string]int64{
+			"B": 1,
+		},
+		expectFail: false,
+	}, {
+		description: "Valid map 2",
+		mapping: map[string]int64{
+			"B":   1,
+			"KiB": 1024,
+			"MiB": 1024 * 1024,
+			"GiB": 1024 * 1024 * 1024,
+			"TiB": 1024 * 1024 * 1024 * 1024,
+		},
+		expectFail: false,
+	}, {
+		description: "Valid map 3",
+		mapping: map[string]int64{
+			"B":   1,
+			"KiB": 1024,
+			"MiB": 1024 * 1024,
+			"":    1024 * 1024,
+			"GiB": 1024 * 1024 * 1024,
+			"TiB": 1024 * 1024 * 1024 * 1024,
+		},
+		expectFail: false,
+	}, {
+		description: "Invalid map 1 (empty map)",
+		mapping:     map[string]int64{},
+		expectFail:  true,
+	}, {
+		description: "Invalid map 2 (no mapping to multiplier 1)",
+		mapping: map[string]int64{
+			"KB": 1000,
+			"MB": 1000 * 1000,
+			"GB": 1000 * 1000 * 1000,
+		},
+		expectFail: true,
+	}, {
+		description: "Invalid map 3 (contains mapping to multiplier 0)",
+		mapping: map[string]int64{
+			"Bad": 0,
+			"B":   1,
+			"KiB": 1024,
+			"MiB": 1024 * 1024,
+			"GiB": 1024 * 1024 * 1024,
+			"TiB": 1024 * 1024 * 1024 * 1024,
+		},
+		expectFail: true,
+	},
+}
+
 var unitTests = []struct {
 	in, out      string
 	value        int64
@@ -25,6 +83,10 @@ var unitTests = []struct {
 	}, {
 		// With invalid unit
 		in:  "23K",
+		err: true,
+	}, {
+		// Out of range value * unit multiplication
+		in:  "18014398509482034KiB",
 		err: true,
 	}, {
 		// With valid unit
@@ -54,6 +116,18 @@ var unitTests = []struct {
 		value:        -1024 * 1024,
 		explicitSign: Negative,
 	},
+}
+
+// TestNewUnit tests the NewUnit function
+func TestNewUnit(t *testing.T) {
+	for _, test := range mapTests {
+		_, err := NewUnit(test.mapping)
+		if test.expectFail && err == nil {
+			t.Fatalf("Test case \"%s\" did not return an error as expected\n", test.description)
+		} else if !test.expectFail && err != nil {
+			t.Fatalf("Test case \"%s\" return an unexpected error: %v\n", test.description, err)
+		}
+	}
 }
 
 // TestUnit implements a table-driven test.
